fac5cf85ea7f142123b93944d0d4f7340a2c511d
Toni <antonio.nascimento@unico.io>
2023-04-10 15:40:12 -0300

    initial structure

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/afjunior/Desktop/dir/nuxt-demo/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
